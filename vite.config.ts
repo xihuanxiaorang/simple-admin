2745b812ca7d26b8e88e4abf4998d32253d743d1
xiaorang <15019474951@163.com>
2024-10-08 22:09:37 +0800

    build: 添加 eslint
    进行代码检查、修复和格式化，用于保持代码风格的一致性和提高代码质量

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
@@ -14,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
