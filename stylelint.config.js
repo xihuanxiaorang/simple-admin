8c4c8aed13f5a804b82dad91163e2521c37a5359
xiaorang <15019474951@163.com>
2024-10-08 22:41:52 +0800

    build: 添加 Stylelint 校验
    用于规避 CSS 代码中的错误并保持一致的编码风格

---
+++ stylelint.config.js
@@ -0,0 +1,40 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-recess-order',
+  ],
+  overrides: [
+    {
+      files: ['**/*.(vue|html)'],
+      customSyntax: 'postcss-html',
+    },
+    {
+      files: ['**/*.scss'],
+      customSyntax: 'postcss-scss',
+    },
+  ],
+  rules: {
+    // 允许 global 、export 、v-deep等伪类
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global', 'export', 'v-deep', 'deep'],
+      },
+    ],
+    'unit-no-unknown': [
+      true,
+      {
+        ignoreUnits: ['rpx'],
+      },
+    ],
+    'comment-empty-line-before': 'never',
+    'custom-property-empty-line-before': 'never',
+    'no-empty-source': null,
+    'comment-no-empty': null,
+    'no-duplicate-selectors': null,
+    'scss/comment-no-empty': null,
+    'selector-class-pattern': null,
+    'font-family-no-missing-generic-family-keyword': null,
+  },
+}
