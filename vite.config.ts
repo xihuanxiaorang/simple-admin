033e9cf9c1c61c5ca79752f83ef8923450cbf348
xiaorang <15019474951@163.com>
2024-10-09 23:02:24 +0800

    feat: 引入ElementPlus并配置自动导入api以及组件

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+const pathSrc = resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +17,33 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [
+        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+        ElementPlusResolver(),
+      ],
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      // 自动导入 VueRouter 相关函数，如：useRouter 等
+      // 自动导入 Pinia 相关函数，如：createPinia，defineStore，storeToRefs 等
+      // 参考自： https://github.com/sxzz/element-plus-best-practices/blob/main/vite.config.ts
+      imports: ['vue', 'vue-router', 'pinia'],
+      // 是否在 vue 模板中自动导入
+      vueTemplate: true,
+      // 指定自动导入函数TS类型声明文件路径，为true时在项目根目录自动创建，为false时关闭自动生成
+      dts: resolve(pathSrc, 'typings', 'auto-imports.d.ts'),
+    }),
+    Components({
+      resolvers: [
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+      ],
+      // 组件名称包含目录，防止同名组件冲突
+      directoryAsNamespace: true,
+      // 指定自定义组件位置(默认:src/components)
+      dirs: ['src/components', 'src/**/components'],
+      // 指定自动导入组件TS类型声明文件路径，为true时在项目根目录自动创建，为false时关闭自动生成
+      dts: resolve(pathSrc, 'typings', 'components.d.ts'),
+    }),
   ],
   resolve: {
     alias: {
